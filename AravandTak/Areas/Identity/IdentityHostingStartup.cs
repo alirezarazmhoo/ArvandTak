8e0602e94c9e6d5486aaf8611c559a74d82156b4
alirezarazmhoo <alireza1539razmjoo@yahoo.com>
2021-05-24 11:27:39 +0430

    Initial

---
+++ AravandTak/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AravandTak.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AravandTak.Areas.Identity.IdentityHostingStartup))]
+namespace AravandTak.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
