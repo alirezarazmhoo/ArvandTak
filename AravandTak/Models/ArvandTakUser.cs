e12687b9ee5a7cfa3b92d2d0263e8d3cfe5a9b45
alirezarazmhoo <alireza1539razmjoo@yahoo.com>
2021-05-24 17:06:11 +0430

    Ui Copied

--- AravandTak/Models/ArvandTakUser.cs
+++ AravandTak/Models/ArvandTakUser.cs
@@ -4,6 +4,10 @@ namespace AravandTak.Models
 {
 	public class ArvandTakUser : IdentityUser
 	{
-
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string ReferenceUserId { get; set; }
+		public int AddressId { get; set; }
+		public string Code { get; set; }
 	}
 }
